c11cdcfd62c2ce8440a34ed2bcd18cbe22d5b4ab
filip <fiqxio@gmail.com>
2016-03-07 01:07:55 +0100

    updated views, auth code

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var apiRouter = require('./api.js');
 var confessionModel = require('./models/confession.js');
+var crypto = require('crypto');
+
 const _port = 1337;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,18 +15,22 @@ app.use('/api', apiRouter);
 app.set('view engine', 'jade');
 
 app.get('/', (req, res)=>{
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.render('index');
 });
 app.post('/', (req, res)=>{
   var confession = new confessionModel();
   confession.text = req.body.text;
   confession.embed = req.body.embed;
+  confession.auth = crypto.randomBytes(5).toString('hex');
   // TODO: DELCODE
   console.log(confession);
   confession.save((err)=>{
     if(err) res.send(err);
-      res.json({success: true, response: {message: 'confession created'}});
+      res.render('index', {success: true, confession: confession});
   });
+});
+app.get('/cotojest', (req, res)=>{
+  res.render('cotojest');
 });
 app.listen(_port, ()=>{
   console.log('listening on port '+_port);
--- models/confession.js
+++ models/confession.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var confessionSchema = new Schema({
     text: String,
     embed: String,
+    auth: String,
     entryID: Number,
     accepted: {type: Boolean, default: false}
 });
