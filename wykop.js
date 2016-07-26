5d4e3dcc527272a10987d6c2d9568201464be9f8
filip <fiqxio@gmail.com>
2016-07-26 10:28:02 +0200

    dont crash after connection key gets revoked

--- wykop.js
+++ wykop.js
@@ -1,7 +1,10 @@
 var config = require('./config.js');
 var Wykop = require('wykop-es6');
 var wykop = new Wykop(config.wykop.key, config.wykop.secret);
-wykop.login(config.wykop.connection).then(function(res){
-  console.log('logged In');
-});
+wykop.relogin = function(){
+  wykop.login(config.wykop.connection).then(function(res){
+    console.log('logged In');
+  });
+}
+wykop.relogin();
 module.exports = wykop;
