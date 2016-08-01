61e9af3bace58c911cd8c041188ec69b380fdd5f
filip <fiqxio@gmail.com>
2016-08-01 13:43:32 +0200

    fix

--- api.js
+++ api.js
@@ -43,10 +43,11 @@ apiRouter.route('/confession/accept/:confession_id').get((req, res)=>{
     var entryBody = `#AnonimoweMirkoWyznania \n${confession.text}\n\n [Kliknij tutaj, aby odpowiedzieć w tym wątku anonimowo](${config.siteURL}/reply/${confession._id}) \n[Kliknij tutaj, aby wysłać OPowi anonimową wiadomość prywatną](${config.siteURL}/conversation/${confession._id}/new) \nPost dodany za pomocą skryptu AnonimoweMirkoWyznania ( ${config.siteURL} ) Zaakceptował: ${req.decoded._doc.username} \n **Po co to?** \n Dzięki temu narzędziu możesz dodać wpis pozostając anonimowym.`;
     wykop.request('Entries', 'Add', {post: {body: entryBody, embed: confession.embed}}, (err, response)=>{
       if(err){
-        if(err.error.code==11){
-          wykop.relogin();
-        }
+        //{"error":{"code":11,"message":"Niepoprawny klucz użytkownika"}}
         res.json({success: false, response: {message: JSON.stringify(err), status: 'warning'}});
+        if(err.error.code==11){
+          throw(err);
+        }
         return;
       }
       confession.entryID = response.id;
